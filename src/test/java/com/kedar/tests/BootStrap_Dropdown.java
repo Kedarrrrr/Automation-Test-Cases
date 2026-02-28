dced1fe2cd772051dbdf97d1759f09e8a37faf5b
Kedarrrrr <132215546+Kedarrrrr@users.noreply.github.com>
2026-02-28 22:29:31 +0530

    hiddedn Dropdown

---
+++ src/test/java/com/kedar/tests/BootStrap_Dropdown.java
@@ -0,0 +1,15 @@
+package com.kedar.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BootStrap_Dropdown {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.jquery-az.com/boots/demo/php?x=63.0_2");
+	}
+
+}

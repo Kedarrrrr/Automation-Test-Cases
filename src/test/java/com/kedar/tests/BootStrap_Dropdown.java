683c4712470f6e12896e8822437c869847d4ec91
Kedarrrrr <132215546+Kedarrrrr@users.noreply.github.com>
2026-03-01 15:04:32 +0530

    Auto suggestions Handling

--- src/test/java/com/kedar/tests/BootStrap_Dropdown.java
+++ src/test/java/com/kedar/tests/BootStrap_Dropdown.java
@@ -1,15 +1,41 @@
 package com.kedar.tests;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BootStrap_Dropdown {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
 		
-		driver.get("https://www.jquery-az.com/boots/demo/php?x=63.0_2");
+		driver.get("https://www.google.com/");
+		
+		driver.findElement(By.xpath("//textarea[@id='APjFqb']")).sendKeys("Kedar");
+		
+		Thread.sleep(3000);
+		
+		List<WebElement> options= driver.findElements(By.xpath("//ul[@role='listbox']/li[@role='presentation']"));
+	
+		System.out.println(options.size());
+		
+		for(WebElement x:options) {
+			String name=(x.getText());
+			if(name.contains("Kedarnath")) {
+				//System.out.println("True");
+				x.click();
+				break;
+			}else {
+				System.out.println(x.getText());
+			}
+		}
+		
 	}
+	
 
 }

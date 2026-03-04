32d72a17ab76a683b11f64bceb609328764becd5
Kedarrrrr <132215546+Kedarrrrr@users.noreply.github.com>
2026-03-04 18:18:45 +0530

    Broken Links Handling

---
+++ src/test/java/com/kedar/tests/Broken_Linkes.java
@@ -0,0 +1,46 @@
+package com.kedar.tests;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Broken_Linkes {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://testautomationpractice.blogspot.com/");
+		
+		List<WebElement> links= driver.findElements(By.tagName("a"));
+		
+		for(WebElement x:links) {
+			
+			String hrefatvalue = x.getAttribute("href");
+			
+			if(hrefatvalue==null || hrefatvalue.isEmpty()) {
+				
+				System.out.print("link is Empty");
+			}
+			URL linkurl = new URL(hrefatvalue);
+			HttpURLConnection conn= (HttpURLConnection) linkurl.openConnection();
+			
+			conn.connect();
+			
+			if(conn.getResponseCode()>=400) {
+				System.out.println("Broken Link");
+			}
+			else {
+				System.out.println("Working Link");
+			}
+			
+		}
+	}
+
+}
